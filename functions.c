619db78f5355540e2440b8f01e93d5e2c1f08df5
yiduoke <yiduokd822@jhs185.org>
2017-10-05 11:26:41 -0400

    something

--- functions.c
+++ functions.c
@@ -52,28 +52,3 @@ char* my_strchr(char *s1, char c){
   //Char not found
   return NULL;
 }
-int main(){
-    printf("%d\n",my_strlen("yes"));
-    char x[5];
-    printf("%s\n",my_strcpy(x,"hello"));
-    char y[10]="snake";
-    printf("%s\n\n",my_strcat(y,"_case"));
-    //MY_STRCMP() TEST
-    printf("Expected: Hello vs Helloo: %d\n", strcmp("Hello", "Helloo"));
-    printf("Result: Hello vs Helloo: %d\n", my_strcmp("Hello", "Helloo"));
-    printf("Expected: Hello vs Stuy: %d\n", strcmp("Hello", "Stuy"));
-    printf("Result: Hello vs Stuy: %d\n", my_strcmp("Hello", "Stuy"));
-    printf("Expected: Hello vs '': %d\n", strcmp("Hello", ""));
-    printf("Result: Hello vs '': %d\n\n", my_strcmp("Hello", ""));
-
-    //MY_STRCHR() TEST
-    printf("Expected: 'H' in Hello: %s\n", strchr("Hello", 'H'));
-    printf("Result: 'H' in Hello: %s\n", my_strchr("Hello", 'H'));
-    printf("Expected: 'l' in Hello: %s\n", strchr("Hello", 'l'));
-    printf("Result: 'l' in Hello: %s\n", my_strchr("Hello", 'l'));
-    printf("Expected: 'i' in Anish: %s\n", strchr("Anish", 'i'));
-    printf("Result: 'i' in Anish: %s\n", my_strchr("Anish", 'i'));
-    printf("Expected: 'j' in Stuy: %s\n", strchr("Stuy", 'j'));
-    printf("Result: 'j' in Stuy: %s\n", my_strchr("Stuy", 'j'));
-    return 1;
-}
