da4f6df9a132a597cf3de5e3ca544d0444407a89
yiduoke <yiduokd822@jhs185.org>
2017-10-05 11:18:36 -0400

    some new files

---
+++ functions.h
@@ -0,0 +1,6 @@
+int my_strlen( char * );
+char *my_strcpy(char *dest, char *source);
+char *my_strcat(char *dest, char *source);
+int my_strcmp(char *s1, char *s2);
+char* my_strchr(char *s1, char c);
+
